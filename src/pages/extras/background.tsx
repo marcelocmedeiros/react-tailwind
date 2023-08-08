1a9b5db874e9ccc0dc300d5bec7030d54d2cc884
marcelocmedeiros <marcelocmdev@gmail.com>
2023-08-08 06:26:29 -0300

    add

---
+++ src/pages/extras/background.tsx
@@ -0,0 +1,11 @@
+export default function Background() {
+    return (
+        <div className="
+            flex-center h-screen
+            bg-transparent
+            bg-gradient-to-r from-purple-950 via-black to-purple-950
+        ">
+            Background
+        </div>
+    )
+}
